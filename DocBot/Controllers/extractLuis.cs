0d4d1aaddcda4947c2a392bdfb8111e6e058d381
gitmenonsandu <sandeep.n.menon@ieee.org>
2016-11-12 18:08:17 +0530

    Luis added

---
+++ DocBot/Controllers/extractLuis.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocBot.Controllers
+{
+    public class ExtractLuis
+    {
+        public string getDisease(LuisResult LuisResponse)
+        {
+            if (LuisResponse.Intents[0].Intent == "haveDisease" && LuisResponse.Intents[0].Intent!="None")
+                return LuisResponse.Entities[0].Entity;
+            return null;
+        }
+    }
+}
